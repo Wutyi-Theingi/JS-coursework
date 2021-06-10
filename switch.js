fdfca149c7401fd5c4bcfb70f3d1612cef253816
Wutyi-Theingi <necyphantom004@gmail.com>
2021-06-11 00:20:35 +0630

    switch js finish

---
+++ switch.js
@@ -0,0 +1,18 @@
+let animeName = "Naruto";
+
+switch (animeName) {
+    case "Boruto":
+        console.log("That's his son series");
+        break;
+    case "BlackClover":
+        console.log("That's not the right one");
+        break;
+    case "Naruto":
+        console.log("Yes that's! My favourite series");
+        break;
+    case "YuruCamp":
+        console.log("Even not the same genres");
+        break;
+    default:
+        console.log("I don't know such anime");
+}
