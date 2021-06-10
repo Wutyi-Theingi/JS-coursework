f46dcd4fad8edcefa50433f5a2ed0aaf7c9b57c5
Wutyi-Theingi <necyphantom004@gmail.com>
2021-06-11 00:24:46 +0630

    ifelse js finish

---
+++ ifelse.js
@@ -0,0 +1,12 @@
+let age = 20;
+if (age < 10) {
+    message = 'Just go play Candy Crash';
+} else if (age < 20) {
+    message = 'You are not still eligible to play this game';
+} else if (age > 20) {
+    message = 'Just go back to work';
+} else if (age = 20) {
+    message = 'Welcome to 999dice';
+} else {
+    message = 'What is this strange age?';
+}
